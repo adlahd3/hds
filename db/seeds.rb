59267bf742645e32844979f1e92efed882055dce
Adel Alahmed <adlahd@hotmail.com>
2014-07-18 05:10:55 +0300

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Role.create([{name:'admin'},{name:'driver'},{name:'frontdesk'},{name:'coordinator'}])
+
+cities = City.create([{name:'Riyadh'}])
+
+District.create(name:'Olaya',city:cities.first)
+
+OrderType.create([{name:'Normal'},{name:'Recurring'}])
+
+Status.create([{name:'open'},{name:'closed'},{name:'force closed'},{name:'cancelled'},{name:'assigned'}])
+
+User.create(name:'Adel Alahmed',email:'adel@alahmed.com',password:'test@1234')
